4987733a87fda7ef6d4fdcb170ed8cccaeb7d6a6
naam <asfasf@gmail.com>
2019-01-29 11:58:54 +0100

    Changed some variables

--- src/com/parkeersim/parkeersim/models/SimulatorModel.java
+++ src/com/parkeersim/parkeersim/models/SimulatorModel.java
@@ -31,11 +31,11 @@ public class SimulatorModel extends BaseModel {
     //todo: als auto's te lang in de queue staan kunnen ze weg gaan
 
     int weekDayArrivals= 130; // average number of arriving cars per hour
-    int weekendArrivals = 220; // average number of arriving cars per hour
+    int weekendArrivals = 250; // average number of arriving cars per hour
     int weekDayPassArrivals= 60; // average number of arriving cars per hour
-    int weekendPassArrivals = 75; // average number of arriving cars per hour
+    int weekendPassArrivals = 80; // average number of arriving cars per hour
     int weekDayReservationArrivals = 30;
-    int weekendReservationArrivals = 35;
+    int weekendReservationArrivals = 40;
 
     int enterSpeed = 3; // number of cars that can enter per minute
     int paymentSpeed = 7; // number of cars that can pay per minute
@@ -60,7 +60,7 @@ public class SimulatorModel extends BaseModel {
                         e.printStackTrace();
                     }
                 } else {
-                    System.out.println(entranceCarQueue.carsInQueue());
+                    //System.out.println(entranceCarQueue.carsInQueue());
                     tick();
                 }
             }
@@ -96,7 +96,7 @@ public class SimulatorModel extends BaseModel {
     }
 
     private void payExpanses(){
-        
+
     }
 
     public int getWeek() {
@@ -206,7 +206,6 @@ public class SimulatorModel extends BaseModel {
         while (queue.carsInQueue()>0 && i<enterSpeed)
         {
             Car car = queue.removeCar();
-            System.out.println(car.getStayTime());
             if(car.getTypeid() == 1 && garagemodel.getNumberOfOpenParkingPassSpots()>0){
                 Location freeLocation = garagemodel.getFirstFreeParkingPassLocation();
                 garagemodel.setCarAt(freeLocation, car);
@@ -214,97 +213,97 @@ public class SimulatorModel extends BaseModel {
                 Location freeLocation = garagemodel.getFirstFreeLocation();
                 garagemodel.setCarAt(freeLocation, car);
             }
-            i++;
+                    i++;
+                }
         }
-    }
 
-    private void carsReadyToLeave(){
-        // Add leaving cars to the payment queue.
-        Car car = garagemodel.getFirstLeavingCar();
-        while (car!=null) {
-            if (car.getHasToPay()){
-                car.setIsPaying(true);
-                paymentCarQueue.addCar(car);
+        private void carsReadyToLeave(){
+            // Add leaving cars to the payment queue.
+            Car car = garagemodel.getFirstLeavingCar();
+            while (car!=null) {
+                if (car.getHasToPay()){
+                    car.setIsPaying(true);
+                    paymentCarQueue.addCar(car);
+                }
+                else {
+                    carLeavesSpot(car);
+                }
+                car = garagemodel.getFirstLeavingCar();
             }
-            else {
+        }
+
+        private void carsPaying(){
+            // Let cars pay.
+            int i=0;
+            while (paymentCarQueue.carsInQueue()>0 && i < paymentSpeed){
+                Car car = paymentCarQueue.removeCar();
+                double priceRounded = Math.round(car.getStayTime() * 0.025 * 100);
+                double price = priceRounded/100;
+                if(car.getTypeid() == 2){
+                    money += price * 1.5;
+                } else {
+                    money += price;
+                }
                 carLeavesSpot(car);
+                i++;
             }
-            car = garagemodel.getFirstLeavingCar();
         }
-    }
 
-    private void carsPaying(){
-        // Let cars pay.
-        int i=0;
-        while (paymentCarQueue.carsInQueue()>0 && i < paymentSpeed){
-            Car car = paymentCarQueue.removeCar();
-            double priceRounded = Math.round(car.getStayTime() * 0.025 * 100);
-            double price = priceRounded/100;
-            if(car.getTypeid() == 2){
-                money += price * 1.5;
+        private void carsLeaving(){
+            // Let cars leave.
+            int i=0;
+            while (exitCarQueue.carsInQueue()>0 && i < exitSpeed){
+                exitCarQueue.removeCar();
+                i++;
+            }
+        }
+
+        /**
+         * Method that calculates an amount of cars that enter a queue
+         * @param weekDay
+         * @param weekend
+         * @return
+         */
+        private int getNumberOfCars(int weekDay, int weekend){
+            Random random = new Random();
+
+            // Get the average number of cars that arrive per hour.
+            int averageNumberOfCarsPerHour;
+            if(day < 5){
+                if (hour < 6){
+                    averageNumberOfCarsPerHour = (weekDay / 10) * 4;
+                } else if(hour < 11){
+                    averageNumberOfCarsPerHour = (weekDay / 10) * 7;
+                } else {
+                    averageNumberOfCarsPerHour = weekDay;
+                }
             } else {
-                money += price;
+                if (hour < 6){
+                    averageNumberOfCarsPerHour = (weekend / 10) * 4;
+                } else if(hour < 11){
+                    averageNumberOfCarsPerHour = (weekend / 10) * 7;
+                } else {
+                    averageNumberOfCarsPerHour = weekend;
+                }
             }
-            carLeavesSpot(car);
-            i++;
-        }
-    }
 
-    private void carsLeaving(){
-        // Let cars leave.
-        int i=0;
-        while (exitCarQueue.carsInQueue()>0 && i < exitSpeed){
-            exitCarQueue.removeCar();
-            i++;
-        }
-    }
-
-    /**
-     * Method that calculates an amount of cars that enter a queue
-     * @param weekDay
-     * @param weekend
-     * @return
-     */
-    private int getNumberOfCars(int weekDay, int weekend){
-        Random random = new Random();
-
-        // Get the average number of cars that arrive per hour.
-        int averageNumberOfCarsPerHour;
-        if(day < 5){
-            if (hour < 6){
-                averageNumberOfCarsPerHour = (weekDay / 10) * 4;
-            } else if(hour < 11){
-                averageNumberOfCarsPerHour = (weekDay / 10) * 7;
-            } else {
-                averageNumberOfCarsPerHour = weekDay;
-            }
-        } else {
-            if (hour < 6){
-                averageNumberOfCarsPerHour = (weekend / 10) * 4;
-            } else if(hour < 11){
-                averageNumberOfCarsPerHour = (weekend / 10) * 7;
-            } else {
-                averageNumberOfCarsPerHour = weekend;
-            }
+            // Calculate the number of cars that arrive this minute.
+            double standardDeviation = averageNumberOfCarsPerHour * 0.3;
+            double numberOfCarsPerHour = averageNumberOfCarsPerHour + random.nextGaussian() * standardDeviation;
+            return (int)Math.round(numberOfCarsPerHour / 60);
         }
 
-        // Calculate the number of cars that arrive this minute.
-        double standardDeviation = averageNumberOfCarsPerHour * 0.3;
-        double numberOfCarsPerHour = averageNumberOfCarsPerHour + random.nextGaussian() * standardDeviation;
-        return (int)Math.round(numberOfCarsPerHour / 60);
-    }
-
-    /**
-     * Method that adds new cars and puts them in the corresponding queue
-     * @param numberOfCars
-     * @param type
-     */
-    private void addArrivingCars(int numberOfCars, String type){
-        // Add the cars to the back of the queue.
-        switch(type) {
-            case AD_HOC:
-                for (int i = 0; i < numberOfCars; i++) {
-                    entranceCarQueue.addCar(new AdHocCar());
+        /**
+         * Method that adds new cars and puts them in the corresponding queue
+         * @param numberOfCars
+         * @param type
+         */
+        private void addArrivingCars(int numberOfCars, String type){
+            // Add the cars to the back of the queue.
+            switch(type) {
+                case AD_HOC:
+                    for (int i = 0; i < numberOfCars; i++) {
+                        entranceCarQueue.addCar(new AdHocCar());
                 }
                 break;
             case PASS:
