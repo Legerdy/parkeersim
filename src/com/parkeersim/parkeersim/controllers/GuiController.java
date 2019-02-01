54abd8206e05f1c22a43ed26fc94fb76c190cedd
Joost <keeperjoost@gmail.com>
2019-02-01 12:42:48 +0100

    added javadoc comments

--- src/com/parkeersim/parkeersim/controllers/GuiController.java
+++ src/com/parkeersim/parkeersim/controllers/GuiController.java
@@ -6,10 +6,24 @@ import com.parkeersim.parkeersim.models.GuiModel;
 
 public class GuiController extends BaseController {
 
+    /**
+     * Constructor of the class GuiController
+     *
+     * @param model
+     */
     public GuiController(GuiModel model) {
         super(model);
     }
 
+    /**
+     * this method is a event handler, it responds to different event_id's
+     * when a event is successfully done, it returns a true boolean,
+     * if not, it returns a false boolean
+     *
+     * @param view
+     * @param event_id
+     * @return boolean
+     */
     @Override
     public boolean event(BaseView view, int event_id){
         GuiModel guimodel = (GuiModel)model;
